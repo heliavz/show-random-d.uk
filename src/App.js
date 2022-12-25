7f3f96e34dfe7bb7412f98abe5433cb3d4ca304b
heliosvz <heliiavalizadeh@gmail.com>
2022-12-25 19:11:02 +0330

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [image,setImg]=useState([])
+  useEffect(()=>{
+    const fetchAPI= async () =>{
+      await axios.get('https://random-d.uk/api/random')
+      .then (response => {
+         setImg(response.data)
+     })} 
+
+    fetchAPI();
+},[])
+
+  return (
+    <div className="App">
+     <img src={image.url} alt="duck/ducks from random-d.uk" />
+    </div>
+  );
+}
+
+export default App;
